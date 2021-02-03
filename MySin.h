7f4638cf9b66dc40e9b0ba1763d1de57c1dc8114
bartek <bartek.krason96@gmail.com>
2021-02-03 11:25:28 +0100

    Doxyfile

---
+++ MySin.h
@@ -0,0 +1,16 @@
+class MySin
+{
+	private:
+		double mX;
+	public:
+		MySin();
+		MySin(double x);
+		MySin(const MySin &obj);
+		~MySin();
+		double value();
+		void setX(double x);
+		double getX() const;
+		unsigned int factorial(unsigned int n);
+		double power(double x, unsigned int y);
+};
+	
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # repo158267
+Egzamin - 03-02-2021- Bartłomiej Krasoń- Egzamin
